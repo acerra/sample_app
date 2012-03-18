8408760c987939024ffdaaef03458b9d75962476
mutandi <acerra86@gmail.com>
2012-03-17 19:51:52 -0500

    finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 SampleApp::Application.routes.draw do
-  resources :users
-
   root to: 'static_pages#home'
-
+  
+  resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+  
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy', :via => :delete
   match '/signup', to: 'users#new'
   
   match '/about', to: 'static_pages#about'
